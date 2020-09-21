259ade20629e1ded1336fbbc9516d68d675dddb0
Rahul Bhatt <rahulbhatt1899@gmail.com>
2020-09-21 23:13:25 +0530

    Changed About.js to Brand.js and similar changes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import ContactPage from './pages/contact';
-import AboutPage from './pages/about';
+import BrandPage from './pages/brand';
 import FaqsPage from './pages/faqs';
 import SupportPage from './pages/support';
 import IndexPage from './pages';
@@ -13,7 +13,7 @@ class App extends Component {
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/contact" component={ContactPage} />
-          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/brand" component={BrandPage} />
           <Route exact path="/faqs" component={FaqsPage} />
           <Route exact path="/support" component={SupportPage} />
           <Route exact path="/" component={IndexPage} />
